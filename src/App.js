672d64be67a13dd33debe205b618b909eb0b554f
Vessaliux <ivessaliux@gmail.com>
2019-08-01 02:16:46 +0800

    react setup & installation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './styles/App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <h1>Hello</h1>
+            <p>This is the React front-end.</p>
+        </div>
+    );
+}
+
+export default hot(module)(App);
